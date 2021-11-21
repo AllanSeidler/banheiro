b03a5fd040e1c7562cb334a9393977a64c252655
AllanSeidler <91341721+AllanSeidler@users.noreply.github.com>
2021-11-20 23:51:36 -0300

    Upload de arquivos

---
+++ Principal.java
@@ -0,0 +1,28 @@
+package banheiro;
+
+public class Principal {
+
+    public static void main(String[] args) {
+
+        Banheiro banheiro = new Banheiro();
+
+        //Passando a tarefa e o nome do Thread
+        Thread convidado1 = new Thread(new TarefaNumero1(banheiro), "João");
+        Thread convidado2 = new Thread(new TarefaNumero2(banheiro), "Pedro");
+      //outros threads
+        Thread convidado3 = new Thread(new TarefaNumero1(banheiro), "Maria");
+        Thread convidado4 = new Thread(new TarefaNumero2(banheiro), "Ana");
+        
+      //outros threads omitidos
+        Thread limpeza = new Thread(new TarefaLimpeza(banheiro), "Limpeza");
+
+        limpeza.setDaemon(true);
+        //inicialização dos outros threads omitida
+        limpeza.start();
+
+        convidado1.start();
+        convidado2.start();
+        convidado3.start();
+        convidado4.start();
+    }
+}
